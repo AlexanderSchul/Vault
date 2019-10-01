ea24aa6f4ba96e8792d6d159bd361bdd2dfc56f6
AlexanderSchul <alex.schul@hotmail.de>
2019-10-01 11:35:34 +0200

    removing v2

--- SchereSteinPapier/SchereSteinPapier/Program.cs
+++ SchereSteinPapier/SchereSteinPapier/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace SchereSteinPapier
 {
-    class Programv1
+    class Program
     {
-        
+
         enum Moves
         {
             Schere = 1,
@@ -19,13 +19,9 @@ namespace SchereSteinPapier
         }
         static void Main(string[] args)
         {
-            string PlayerName;
 
-            
+            int[] Data = new int[3];
 
-            string PlayerMove;
-
-            string[] Moves = new string[5] { "Schere", "Stein", "Papier", "Echse", "Spock" };
             Console.WriteLine("Willkommen bei Schere, Stein, Papier, Echse, Spock");
             Console.WriteLine("Die Spielregeln lauten:");
             Console.WriteLine("Schere schneidet Papier");
@@ -40,175 +36,193 @@ namespace SchereSteinPapier
             Console.WriteLine("Stein zertrümmert Schere");
             Console.WriteLine("_____________________________");
             Console.Write("Gib deinen Spielernamen ein: ");
-            PlayerName = Console.ReadLine();
-            Random rnd = new Random();
-            Game();
+            string PlayerName = Console.ReadLine();
 
-
-            void Game()
+            do
             {
-                string Continue = "";
-
-                while (Continue != "N")
+                Console.WriteLine("Gib deinen Zug ein (1 = Schere, 2 = Stein, 2 = Papier, 4 = Echse, 5 = Spock, 0 beendet das Spiel): ");
+                bool check = int.TryParse(Console.ReadLine(), out int playermove);
+                if (check == true)
                 {
+                    Array.Copy(Game(playermove), 0, Data, 0, 3);
 
-                    int PCmoveN = rnd.Next(0, 4);
-                    Continue = "";
-                    Console.WriteLine("Gib deinen Zug ein: ");
-                    PlayerMove = Console.ReadLine();
-                    //Console.WriteLine("Computer move is: " + Moves[PCmoveN]);
-
-                    if (PlayerMove == Moves[PCmoveN])
+                    if (Data[2] == 1)
                     {
-                        Console.WriteLine("Gleicher Zug - Runde wird wiederholt");
-                        Continue = "Y";
+                        Console.WriteLine("Computer spielt " + Enum.GetName(typeof(Moves), Data[1]) + " und " + PlayerName + " spielt " + Enum.GetName(typeof(Moves), Data[0]));
+                        Console.WriteLine(Enum.GetName(typeof(Moves), Data[0]) + " schlägt " + Enum.GetName(typeof(Moves), Data[1]));
+                        Console.WriteLine(PlayerName + " gewinnt.");
+                        Console.WriteLine("____________________________________________________________");
                     }
-
-                    else if ((PlayerMove == "Schere" || PlayerMove == "schere") && Moves[PCmoveN] == "Papier")
+                    else if (Data[2] == 0)
                     {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Schere schneidet Papier - " + PlayerName + " gewinnt");
+                        Console.WriteLine("Computer spielt " + Enum.GetName(typeof(Moves), Data[1]) + " und " + PlayerName + " spielt " + Enum.GetName(typeof(Moves), Data[0]));
+                        Console.WriteLine(Enum.GetName(typeof(Moves), Data[1]) + " schlägt " + Enum.GetName(typeof(Moves), Data[0]));
+                        Console.WriteLine("Computer gewinnt.");
+                        Console.WriteLine("____________________________________________________________");
                     }
-
-
-                    else if ((PlayerMove == "Papier" || PlayerMove == "papier") && Moves[PCmoveN] == "Schere")
+                    else if (Data[2] == 2)
                     {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Schere schneidet Papier - Computer gewinnt");
+                        Console.WriteLine("Computer spielt " + Enum.GetName(typeof(Moves), Data[1]) + " und " + PlayerName + " spielt " + Enum.GetName(typeof(Moves), Data[0]));
+                        Console.WriteLine("Gleicher Zug, Runde wird wiederholt.");
+                        Console.WriteLine("____________________________________________________________");
                     }
-
-
-                    else if ((PlayerMove == "Papier" || PlayerMove == "papier") && Moves[PCmoveN] == "Stein")
+                    else if (Data[2] == 4)
                     {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Papier bedeckt Stein - " + PlayerName + " gewinnt");
+                        Console.WriteLine("Ungültiger Zug. Gib eine Zahl zwischen 0 und 5 ein.");
+                        Console.WriteLine("____________________________________________________________");
                     }
-
-                    else if ((PlayerMove == "Stein" || PlayerMove == "stein") && Moves[PCmoveN] == "Papier")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Papier bedeckt Stein - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Stein" || PlayerMove == "stein") && Moves[PCmoveN] == "Echse")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Stein zerquetscht Echse - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Echse" || PlayerMove == "echse") && Moves[PCmoveN] == "Stein")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Stein zerquetscht Echse - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Echse" || PlayerMove == "echse") && Moves[PCmoveN] == "Spock")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Echse vergited Spock - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Spock" || PlayerMove == "spock") && Moves[PCmoveN] == "Echse")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Echse vergited Spock - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Spock" || PlayerMove == "spock") && Moves[PCmoveN] == "Schere")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Spock zertrümmert Schere - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Schere" || PlayerMove == "schere") && Moves[PCmoveN] == "Spock")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Spock zertrümmert Schere - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Schere" || PlayerMove == "schere") && Moves[PCmoveN] == "Echse")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Schere köpft Echse - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Echse" || PlayerMove == "echse") && Moves[PCmoveN] == "Schere")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Schere köpft Echse - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Echse" || PlayerMove == "echse") && Moves[PCmoveN] == "Papier")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Echse frisst Papier - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Echse" || PlayerMove == "echse") && Moves[PCmoveN] == "Papier")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Echse frisst Papier - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Papier" || PlayerMove == "papier") && Moves[PCmoveN] == "Spock")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Papier wiederlegt Spock - " + PlayerName + " gewinnt");
-                    }
-                    else if ((PlayerMove == "Spock" || PlayerMove == "spock") && Moves[PCmoveN] == "Papier")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Papier wiederlegt Spock - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Spock" || PlayerMove == "spock") && Moves[PCmoveN] == "Stein")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Spock verdampft Stein - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Stein" || PlayerMove == "stein") && Moves[PCmoveN] == "Spock")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Spock verdampft Stein - Computer gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Stein" || PlayerMove == "stein") && Moves[PCmoveN] == "Schere")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Stein zertrümmert Schere - " + PlayerName + " gewinnt");
-                    }
-
-                    else if ((PlayerMove == "Schere" || PlayerMove == "schere") && Moves[PCmoveN] == "Stein")
-                    {
-                        Console.WriteLine("Computer spielt " + Moves[PCmoveN]);
-                        Console.WriteLine("Stein zertrümmert Schere - Computer gewinnt");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("ungültiger Zug");
-                    }
-                    if (Continue != "Y")
-                    {
-                        Console.WriteLine("_______________________________________________");
-                        Console.WriteLine("Enter zum weiterspielen. 'N' beendet das Spiel.");
-                        Continue = Console.ReadLine();
-                    }
-
                 }
+                else
+                {
+                    Console.WriteLine("Ungültiger Zug. Gib eine Zahl zwischen 0 und 5 ein.");
+                }
+
+            } while (Data[2] != 3);
+
+            Console.WriteLine("Auf Wiedersehen " + PlayerName + "!");
+            Console.ReadKey();
+
+
+        }
+
+
+        static Array Game(int Pmove)
+        {
+            Random rnd = new Random();
+            int[] gameData = new int[3];
+            int PCmove = rnd.Next(1, 5);
+
+            switch (Pmove)                   // gameData[2]: 0=loss, 1=win, 2=same, 3=end game, 4= invalid
+            {
+                case 0:
+                    gameData[2] = 3;
+                    break;
+
+                case 1:
+                    if (PCmove == 1)
+                    {
+                        gameData[2] = 2;
+                    }
+                    else if (PCmove == 2)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 3)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 4)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 5)
+                    {
+                        gameData[2] = 0;
+                    }
+                    break;
+
+                case 2:
+                    if (PCmove == 1)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 2)
+                    {
+                        gameData[2] = 2;
+                    }
+                    else if (PCmove == 3)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 4)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 5)
+                    {
+                        gameData[2] = 0;
+                    }
+                    break;
+
+                case 3:
+                    if (PCmove == 1)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 2)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 3)
+                    {
+                        gameData[2] = 2;
+                    }
+                    else if (PCmove == 4)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 5)
+                    {
+                        gameData[2] = 0;
+                    }
+                    break;
+
+                case 4:
+                    if (PCmove == 1)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 2)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 3)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 4)
+                    {
+                        gameData[2] = 2;
+                    }
+                    else if (PCmove == 5)
+                    {
+                        gameData[2] = 1;
+                    }
+                    break;
+
+                case 5:
+                    if (PCmove == 1)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 2)
+                    {
+                        gameData[2] = 1;
+                    }
+                    else if (PCmove == 3)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 4)
+                    {
+                        gameData[2] = 0;
+                    }
+                    else if (PCmove == 5)
+                    {
+                        gameData[2] = 2;
+                    }
+                    break;
+
+                default:
+                    gameData[2] = 4;
+                    break;
 
             }
 
+            gameData[0] = Pmove;
+            gameData[1] = PCmove;
 
-
-
-            Console.WriteLine("Auf Wiedersehen " + PlayerName);
-
-            
-
-            Console.ReadKey();
-
+            return gameData;
 
 
         }
